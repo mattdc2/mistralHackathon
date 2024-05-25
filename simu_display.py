ca3e6887729add681037982bac997ed8a0537002
Lucas <lucas@MacBook-Pro-de-Lucas-2.local>
2024-05-25 20:31:06 +0200

    simu v2

--- simu_display.py
+++ simu_display.py
@@ -6,13 +6,11 @@
 #    By: lucas <lucas@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/05/25 15:15:24 by lucas             #+#    #+#              #
-#    Updated: 2024/05/25 18:14:21 by lucas            ###   ########.fr        #
+#    Updated: 2024/05/25 20:15:42 by lucas            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import pygame
-
-
 
 
 # parser
@@ -25,14 +23,6 @@ def read_map(filename):
     return map_data
 
 
-
-
-
-
-
-
-
-
 # Classe Voiture
 class Voiture_F:
     def __init__(self, x, y, vitesse, orientation):
@@ -41,7 +31,8 @@ class Voiture_F:
         self.vitesse = vitesse
         self.orientation = orientation  # 0: Nord, 1: Est, 2: Sud, 3: Ouest
 
-    def move(self):
+    # keyboard_kemovemove the car with WASD
+    def keyboard_move(self):
         if self.orientation == 0:  # Nord
             self.y += 1
             self.x = self.x
@@ -58,14 +49,38 @@ class Voiture_F:
             self.x += 1
             self.y = self.y
 
+    # draw the front of the car (red square)
     def draw(self, window, cell_size):
         car_width = cell_size
         car_len = cell_size
         car_rect = pygame.Rect(self.x * cell_size + cell_size // 4, self.y * cell_size + cell_size // 4, car_width, car_len)
         pygame.draw.rect(window, (255, 0, 0), car_rect)
 
+    # keyboard_kemovemove the car with the 5 case that LLM is returning 
+    def interpreter(self, retour):
+        # 0 = stop
+        # 1 = go up
+        # 2 = go down
+        # 3 = go left
+        # 4 = go right
+        if retour == 0:
+            self.vitesse = 0
+            voiture_b.vitesse = 0
+        if retour == 1:
+            self.y += 1
+            self.x = self.x
+        if retour == 2:
+            self.y -= 1
+            self.x = self.x
+        if retour == 3:
+            self.x += 1
+            self.y = self.y
+        if retour == 4:
+            self.x -= 1
+            self.y = self.y
 
-# Classe Voiture
+            
+# Back car class
 class Voiture_B:
     def __init__(self, x, y, vitesse, orientation):
         self.x = x
@@ -73,7 +88,8 @@ class Voiture_B:
         self.vitesse = vitesse
         self.orientation = orientation  # 0: Nord, 1: Est, 2: Sud, 3: Ouest
 
-    def move(self):
+    # keyboard_kemovemove the car with WASD (can be optimized it's always the same moove, the B one take the place of the A one)
+    def keyboard_move(self):
         if self.orientation == 0:  # Nord
             self.x = voiture_f.x
             self.y = voiture_f.y
@@ -86,18 +102,37 @@ class Voiture_B:
         elif self.orientation == 3:  # Ouest
             self.y = voiture_f.y
             self.x = voiture_f.x
-            
-    def draw(self, window, cell_size):
-        # Dessiner la voiture comme un rectangle rouge
+    
+    def interepreteur(self, retour):
+        # 0 = stop
+        # 1 = go up
+        # 2 = go down
+        # 3 = go left
+        # 4 = go right
+        if retour == 0: 
+            self.x = self.x
+            self.y = self.y
+        elif retour == 1:
+            self.y = voiture_f.y
+            self.x = voiture_f.x
+        elif retour == 2:
+            self.y = voiture_f.y
+            self.x = voiture_f.x
+        elif retour == 3:
+            self.y = voiture_f.y
+            self.x = voiture_f.x
+        elif retour == 4:
+            self.x = voiture_f.x
+            self.y = voiture_f.y
+    
+    # draw the back of the car (green square)
+    def draw(self, window, cell_size):    
         car_width = cell_size
         car_len = cell_size
-        # elif self.orientation == 1 or self.orientation == 3:
-        #     car_len = cell_size
-        #     car_width = car_len * 2
         car_rect = pygame.Rect(self.x * cell_size + cell_size // 4, self.y * cell_size + cell_size // 4, car_width, car_len)
         pygame.draw.rect(window, (0, 255, 0), car_rect)
 
-
+    
 
 
 # Init
@@ -113,30 +148,26 @@ pygame.display.set_caption("Simulation de Ville")
 colors = {
     0: (0, 0, 0),        # Noir (route)
     1: (255, 255, 255),  # Blanc (autre)
-    2: (0, 0, 255), 
+    2: (0, 0, 255),        # panneaux
     3: (100, 130, 110),   # Border
     4: (255, 0, 0),      # Stop
 }
-
-
 
 # read
 map_data = read_map('map.txt')
 map_size = 9
 
 
-
 # pixel size
 cell_size = window_size[0] // map_size // 4
-
-
 
 
 # Initialiser une voiture
 voiture_f = Voiture_F(16.75, 0.75, 0.25, 0)  # Position initiale (1, 1), vitesse 0.02, orientation Est = 1 
 voiture_b = Voiture_B(voiture_f.x, voiture_f.y - 1, voiture_f.vitesse, voiture_f.orientation)
 
-# main
+
+# main loop
 running = True
 while running:
     for event in pygame.event.get():
@@ -146,33 +177,20 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_s:
                 voiture_f.orientation = 0  # Nord
-                voiture_b.move()
-                voiture_f.move()
+                voiture_b.keyboard_move()
+                voiture_f.keyboard_move()
             elif event.key == pygame.K_a:
                 voiture_f.orientation = 1  # Est
-                voiture_b.move()
-                voiture_f.move()
+                voiture_b.keyboard_move()
+                voiture_f.keyboard_move()
             elif event.key == pygame.K_w:
                 voiture_f.orientation = 2  # Sud
-                voiture_b.move()
-                voiture_f.move()
+                voiture_b.keyboard_move()
+                voiture_f.keyboard_move()
             elif event.key == pygame.K_d:
                 voiture_f.orientation = 3  # Ouest
-                voiture_b.move()
-                voiture_f.move()
-            elif event.key == pygame.K_g:
-                if voiture_f.orientation == voiture_b.orientation == 0:  #N
-                    voiture_f.y += 1
-                    voiture_b.y += 1
-                elif voiture_f.orientation == voiture_b.orientation == 1: #E
-                    voiture_f.x -= 1
-                    voiture_b.y += 1
-                elif voiture_f.orientation == voiture_b.orientation == 2: #S
-                    voiture_f.y -= 1
-                    voiture_b.y += 1
-                elif voiture_f.orientation == voiture_b.orientation == 3: #W
-                    voiture_f.x += 1
-                    voiture_b.y += 1
+                voiture_b.keyboard_move()
+                voiture_f.keyboard_move()
             elif event.key == pygame.K_SPACE:
                 voiture_f.x = 16.75
                 voiture_f.y = 0.75
@@ -193,6 +211,7 @@ while running:
     # draw car
     voiture_b.draw(window, cell_size)
     voiture_f.draw(window, cell_size)
+    
     # Refresh
     pygame.display.flip()
 
